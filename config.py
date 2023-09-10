246f30420f91d72e60f46eae925384022970d480
kfcuncle <86586814+kfcuncle@users.noreply.github.com>
2023-09-10 17:38:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.cgnhhjdy7gio.us-east-1.rds.amazonaws.com"
+customhost = "database-2.cgnhhjdy7gio.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
